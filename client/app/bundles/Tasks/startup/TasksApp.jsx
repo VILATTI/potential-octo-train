c48d9a29be1835174803f7f375b7ef4835a4c4e5
Sergey Rudik <vilatti@rubygarage.org>
2017-05-15 22:43:50 +0300

    fix TasksApp

--- client/app/bundles/Tasks/startup/TasksApp.jsx
+++ client/app/bundles/Tasks/startup/TasksApp.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import configureStore from '../stores/TasksStore';
-import TasksContainer from '../containers/tasksContainer';
+import TasksContainer from '../containers/TasksContainer';
 
 // See documentation for https://github.com/reactjs/react-redux.
 // This is how you get props from the Rails view into the redux store.
